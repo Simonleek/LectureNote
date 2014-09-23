2f58f9c639d680db85b24ef90de00459ed15e8bc
Simon Lee <simonlee@g.harvard.edu>
2014-09-23 00:04:58 -0400

    add time of day practic

---
+++ timeoftheday.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <title>time of the day</title>
+	<?php require 'datetimelogic.php'; ?>
+</head>
+	<body  bgcolor="<?php echo $backcolor; ?>">
+		<p>
+		 <img src="<?php echo $imageNumber; ?>">
+		</p>
+	</body>
+</html>
